04e24b9da6b0b0c202e5a64ad3ef9e14a4e808bc
Imad Essadeq <imad.essadeq@alten.nl>
2020-01-07 09:54:01 +0100

    Commit

--- cypress/integration/QAMdemo/TC01_basics.spec.js
+++ cypress/integration/QAMdemo/TC01_basics.spec.js
@@ -3,7 +3,7 @@ describe("Login", function(){
         cy.visit('https://react-redux.realworld.io/#/login')
         cy.get('input[type="email"]').type('imadessadeq1@hotmail.com')
         cy.get('input[type="password"]').type('Herfst2019!')//
-        cy.get('.btn').contains('Sign in').should('be.visible').click()
+        cy.get('.btn').contains('Sign in').should('be.visible').click()//
     })
 
 })
